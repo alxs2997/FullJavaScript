e71f42f41d146e35bf1ca71adbb20a2e10f8c2c2
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-06-13 20:28:00 -0600

    haciendo que se escriba dentro del documento html

--- Ejercicios_Practicas/ejercicio2/ejercicio2.js
+++ Ejercicios_Practicas/ejercicio2/ejercicio2.js
@@ -5,6 +5,11 @@ let double = '"comillas dobles"'
 alert(mensaje + " Qué fácil es incluir 'comillas simples' y " + double );
 
 // array de meses que se muestren en consola
-Array = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio","agosto","septiembre","octubre","noviembre","diciembre"]
+Array = ["enero ", "febrero ", "marzo ", "abril ", "mayo ", "junio ", "julio ","agosto ","septiembre ","octubre ","noviembre ","diciembre "]
 
-console.log(Array);
+//que se muestre en consola: 
+
+//console.log(Array);
+
+//que se escriba dentro del body
+document.write(Array);
