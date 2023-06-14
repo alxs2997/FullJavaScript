3bcbb32064cfe32e1a7f3fd56f1a00aa9deafc1c
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-06-13 18:37:18 -0600

    Aprendiendo a declarar y llamar una función desde html

---
+++ funciones/funciones.js
@@ -0,0 +1,11 @@
+//Un programa grande funciona con muchas funciones pequeñitas
+
+// Funciones declarativas
+/*function saludo(contiene parametros) {
+    // codigo a ejecutar
+};*/
+
+function saludar() {
+    alert("Hola, cómo estás?"); //cada vez que querramos usar la función tenemos que llamarla
+};
+//se llama a la función al hacer click en un boton en el archivo html
