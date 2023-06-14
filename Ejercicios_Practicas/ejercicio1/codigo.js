ce90dcb4ca4aeca1d5c1482481433a63bc6361d2
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-06-13 20:20:14 -0600

    practicando con ejercicios extras

---
+++ Ejercicios_Practicas/ejercicio1/codigo.js
@@ -0,0 +1,4 @@
+//Se coloca el codigo de JavaScript en un archivo aparte para que el archivo html no se vea tan cargado
+//y sea mas facil de leer y modificar
+console.log("Hola Mundo!"); //Muestra un mensaje en la consola
+alert("Soy el primer script </>")//Muestra un mensaje en una cuadro de texto emergente
---
+++ Ejercicios_Practicas/ejercicio2/ejercicio2.js
@@ -0,0 +1,10 @@
+//Mostrar un mensaje de alerta con el texto “Hola Mundo!”
+//Utilizar en un texto concatenado las comillas simples y dobles
+var mensaje = "Hola Mundo!";
+let double = '"comillas dobles"'
+alert(mensaje + " Qué fácil es incluir 'comillas simples' y " + double );
+
+// array de meses que se muestren en consola
+Array = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio","agosto","septiembre","octubre","noviembre","diciembre"]
+
+console.log(Array);
