28ee9b26446250ad2484b4c5c086b026e0b9e8e7
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-06-21 10:59:53 -0600

    modificando,reemplazando y eliminando Nodos del DOM

--- DOM-Manipulación/dom.js
+++ DOM-Manipulación/dom.js
@@ -5,7 +5,7 @@ var elemento = document.createElement("li"),
 //se agrega el texto al elemento
     elemento.appendChild(producto);
 //se agrega el elemento a la listavia el id de la lista
-var padre = document.getElementById("lista1");
+//var padre = document.getElementById("lista1");
 //se agrega el elemento a la lista en el html
 padre.appendChild(elemento);
 
@@ -32,3 +32,24 @@ elemento2.style.color = "red";
 //y del elemento agregado al final en la primera lista
 
 elemento.style.color = "blue";
+
+
+//modificando un elemento ya existente
+//var primerElemento = document.getElementById("lista1");
+
+//primerElemento.textContent = "Pera <i><b>con</b> crema</i>"; //esto ejecuta como texto plano el html
+
+//para acceder y cambiar el elemento uno de la lista 1 podemos ocupar innerHTML
+
+//primerElemento.innerHTML = "Pera <i class='foo'><b>con</b> crema</i>"; //en cambio con innerHTML se ejecuta el código en función de html
+
+var elementiun = document.createTextNode("Pera");
+/*var padre3 = document.getElementsByTagName("li")[4].parentNode,
+referencia = document.getElementsByTagName("li")[3];
+
+padre3.replaceChild(elementiun, referencia);*/
+var padre = document.getElementById("lista1");
+var padre = document.getElementsByTagName("li")[2].parentNode;
+
+//eliminando un elemento
+padre.remove(document.getElementsByTagName("li")[2]);
