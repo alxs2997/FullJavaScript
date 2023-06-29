345354b1b2c008f865141e7fa89125cc698fc460
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-06-29 09:37:39 -0600

    propiedades disponibles en XMLHttpRequest

--- AJAX/Object-htmlrequest.js
+++ AJAX/Object-htmlrequest.js
@@ -35,3 +35,40 @@ xhr.send('nombre=Juan&apellido=Perez');
 
 //Estados y propiedades del objeto XMLHttpRequest
 
+//XMLHttpRequest.readyState - devuelve el estado del objeto XMLHttpRequest
+
+//El estado inicial es el que se representa con el numero 0
+
+//readyState = 0 - request no inicializado
+//readyState = 1 - conexion con el servidor establecida
+//readyState = 2 - request recibido
+//readyState = 3 - procesando request
+//readyState = 4 - request finalizado y la respuesta esta lista
+
+//Otra propiedad disponible en XMLHttpRequest es:
+
+//onreadystatechange - define una funcion que sera llamada cuando el estado del objeto cambie
+//esta función se ejecuta cada vez que el estado del objeto cambia con readyState.
+
+//responseText - devuelve la respuesta del servidor como un string
+//responseXML - devuelve la respuesta del servidor como un XMLDocument object
+
+//status - devuelve el estado de la respuesta del servidor,
+//status retorna el estado del request en forma de numerom,
+//por ejemplo 404 para not found, 200 para ok, 500 para error interno del servidor
+
+//ejejmplo de uso:
+
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'crear_usuario.php', true);
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var divUser = document.getElementById('registro');
+        divUser.innerHTML = xhr.responseText;
+    }
+
+}
+
+xhr.send(null);
