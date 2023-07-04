0459aa63be49ae14998dcb50789a9df4cc0e6c7d
DAVID ALEXIS GALLEGOS GONZALEZ <smis023316@ugb.edu.sv>
2023-07-03 19:37:06 -0600

    añadiendo inicio y seccionando la página, completando proyecto

--- Ejercicios_Practicas/restaurant_order-css-html/menú_restaurant/app/app.js
+++ Ejercicios_Practicas/restaurant_order-css-html/menú_restaurant/app/app.js
@@ -56,6 +56,28 @@ $(function() {
   
   });
 
+//sección para funcionamiento del carrousel de imágenes
+var int;
+function setInt() {
+    clearInterval(int);
+    int = setInterval(function() {
+        var btns = document.getElementsByName("carousel");
+        for(var i = 0; i < btns.length; i++) {
+            if(btns[i].checked) {
+                btns[i].checked = false;
+                if(i + 1 == btns.length) {
+                    btns[0].checked = true;
+                }
+                else {
+                    btns[i + 1].checked = true;
+                }
+                return;
+            }
+        }
+    }, 5000); // 5000 milliseconds
+}
+setInt();
+
 //evento al hace clic sobre el bontón ordenar ahora (Order Now!)
 let allContainerOrden = document.querySelector('.container-card');
 let containerOrden = document.querySelector('.mostrar');
